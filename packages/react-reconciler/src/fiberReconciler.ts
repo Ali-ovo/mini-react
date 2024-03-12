6540e7c00e55fa56d6db73e5792356e855f3d75c
ali <ali_ovo@qq.com>
2024-03-12 23:13:11 +0800

    feat: fix interface error

--- packages/react-reconciler/src/fiberReconciler.ts
+++ packages/react-reconciler/src/fiberReconciler.ts
@@ -1,7 +1,7 @@
 import { Container } from 'hostConfig'
 import { FiberNode, FiberRootNode } from './fiber'
 import { HostRoot } from './workTags'
-import { UpdateQueue, createUpdate, createUpdateQueue, enqueueUpdate } from './qudateQueue'
+import { UpdateQueue, createUpdate, createUpdateQueue, enqueueUpdate } from './updateQueue'
 import { ReactElementType } from 'shared/ReactTypes'
 import { scheduleUpdateOnFiber } from './workLoop'
 
